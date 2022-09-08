2cdc8e93c9003ab5e4e6425c0cdf2efee175e16d
paulo rosero <pcrmrr@gmail.com>
2022-09-07 19:42:42 -0500

    primer commit

---
+++ MascotaFeliz.App/MascotaFeliz.App.Dominio/Entidades/Persona.cs
@@ -0,0 +1,15 @@
+using System;
+namespace MascotaFeliz.App.Dominio
+{
+    public class Persona 
+    {
+        public int Id {get;set;} //propiedad que encapsula el atributo de la clase 
+        public string Nombres {get;set;}
+        public string Apellidos {get;set;}
+        public string Direccion {get;set;}
+        public string Telefono {get;set;}
+    }
+
+
+
+}
